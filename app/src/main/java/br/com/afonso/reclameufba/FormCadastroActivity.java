01ddf90fc6552f8a5c294cf4d496b760d7259b95
AfonsoSilva <afonso.aas93@gmail.com>
2017-07-13 23:35:20 -0300

    Criação do projeto e de telas FormPostActivity, FormCadastroActivity, ListPostActivity e LoginActivity

---
+++ app/src/main/java/br/com/afonso/reclameufba/FormCadastroActivity.java
@@ -0,0 +1,13 @@
+package br.com.afonso.reclameufba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+    }
+}
